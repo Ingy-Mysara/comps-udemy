bedba0281caab775e12fb75a12bd5241b6407b4b
Ingy Mysara <ingymysara99@gmail.com>
2024-11-21 11:26:34 +0200

    Prop Types

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-function App(){
-    return (
-        <div>App</div>
-    )
+import Button from "./Button";
+
+function App() {
+  return (
+    <div>
+      <div>
+        <Button success primary>
+          Click me!
+        </Button>
+      </div>
+      <div>
+        <Button>Buy Now</Button>
+      </div>
+      <div>
+        <Button>See Deal</Button>
+      </div>
+      <div>
+        <Button>Button</Button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
